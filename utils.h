b2dd33957ff6adba6bca658c6d426e5b3a513dee
andorok <andorok@gmail.com>
2024-08-26 11:29:27 +0300

    добавил функции проверки существования каталога и создания каталога

--- utils.h
+++ utils.h
@@ -4,6 +4,10 @@
 
 #ifdef __linux__
 #include <unistd.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #else 
 #include <windows.h>
 #endif
@@ -33,6 +37,48 @@ int getCurrentDir(char *buf, int size)
 	if (!ret)
 		return -1;
 	return 0;
+}
+
+//-----------------------------------------------------------------------------
+int isDir(const char* dirname)
+{
+	int ret = 0;
+#ifdef __linux__
+	DIR* dir = opendir(dirname);
+	if (dir)
+	{ // Directory exists
+		closedir(dir); ret = 1;
+	}
+	else
+		if (ENOENT == errno) { // Directory does not exist.
+			ret = 0;
+		}
+		else {  // opendir() failed for some other reason.
+			ret = -1;
+		}
+#else
+	DWORD dwAttrib = GetFileAttributes(dirname);
+	ret = (dwAttrib != INVALID_FILE_ATTRIBUTES &&
+		(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+#endif
+	return ret;
+}
+
+//-----------------------------------------------------------------------------
+void createDir(const char* dirname)
+{
+#ifdef __linux__
+	struct stat st; // = { 0 };
+	if (stat(dirname, &st) == -1) {
+		mkdir(dirname, 0755);
+	}
+#else
+	DWORD dwAttrib = GetFileAttributes(dirname);
+	int isdir = (dwAttrib != INVALID_FILE_ATTRIBUTES &&
+		(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+	if(!isdir)
+		CreateDirectory(dirname, NULL);
+#endif
 }
 
 #endif	// _UTILS_H_
